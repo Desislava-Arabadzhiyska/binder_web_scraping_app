a6f2130f617d6ff5621364e48b9b2814e1863a66
Desislava-Arabadzhiyska <33028920+Desislava-Arabadzhiyska@users.noreply.github.com>
2022-11-07 17:49:40 +0000

    Update app.R

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,15 +1,15 @@
-library(holepunch)
-write_compendium_description(package = "Your compendium name", 
+#library(holepunch)
+#write_compendium_description(package = "Your compendium name", 
                              description = "Your compendium description")
 # to write a description, with dependencies. Be sure to fill in placeholder text
 
-write_dockerfile(maintainer = "your_name") 
+#write_dockerfile(maintainer = "your_name") 
 # To write a Dockerfile. It will automatically pick the date of the last 
 # modified file, match it to that version of R and add it here. You can 
 # override this by passing r_date to some arbitrary date
 # (but one for which a R version exists).
 
-generate_badge() # This generates a badge for your readme.
+#generate_badge() # This generates a badge for your readme.
 
 # ----------------------------------------------
 # At this time 🙌 push the code to GitHub 🙌
