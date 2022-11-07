da59b1c17849489850bb4aaf2bea9d87412f7a57
Desislava-Arabadzhiyska <33028920+Desislava-Arabadzhiyska@users.noreply.github.com>
2022-11-07 17:15:11 +0000

    Update app.R

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,3 +1,22 @@
+library(holepunch)
+write_compendium_description(package = "Your compendium name", 
+                             description = "Your compendium description")
+# to write a description, with dependencies. Be sure to fill in placeholder text
+
+write_dockerfile(maintainer = "your_name") 
+# To write a Dockerfile. It will automatically pick the date of the last 
+# modified file, match it to that version of R and add it here. You can 
+# override this by passing r_date to some arbitrary date
+# (but one for which a R version exists).
+
+generate_badge() # This generates a badge for your readme.
+
+# ----------------------------------------------
+# At this time 🙌 push the code to GitHub 🙌
+# ----------------------------------------------
+
+
+
 #This app goes through the main Reddit page for the First Law series and collects
 #comments in order to do a basic analysis of the sentiments associated with the text
 #Parts of this script are based on the tutorial by Samer Hijjazi (https://www.youtube.com/watch?v=U1BrIPmhx10)
