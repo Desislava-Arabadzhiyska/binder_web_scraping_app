5483e9dfe596c4b6111199e71b650fd9252943c1
Desislava-Arabadzhiyska <33028920+Desislava-Arabadzhiyska@users.noreply.github.com>
2022-11-08 10:03:14 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,11 +1,11 @@
 #install.packages("devtools")
-#install.packages("tidyverse")#for data tidying etc.
-#install.packages("RSelenium")#for interacting with the webpage
-#install.packages('netstat')#for port connection
-#install.packages('sentimentr') # for sentiment analysis
-#install.packages('quanteda') #further text mining and nlp analysis
-#install.packages('quanteda.textplots') # for textplots
-#install.packages('wesanderson') #nicer visualization
-#install.packages('stm') #structural topic model
-#install.packages('shiny')
-#install.packages('stringi') #needed depandancy to run on binder
+install.packages("tidyverse")#for data tidying etc.
+install.packages("RSelenium")#for interacting with the webpage
+install.packages('netstat')#for port connection
+install.packages('sentimentr') # for sentiment analysis
+install.packages('quanteda') #further text mining and nlp analysis
+install.packages('quanteda.textplots') # for textplots
+install.packages('wesanderson') #nicer visualization
+install.packages('stm') #structural topic model
+install.packages('shiny')
+install.packages('stringi') #needed depandancy to run on binder
