4049b12095fba908846fa54152d31f2fa3ffe84f
Desislava-Arabadzhiyska <33028920+Desislava-Arabadzhiyska@users.noreply.github.com>
2022-11-11 23:11:33 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 install.packages("tidyverse")#for data tidying etc.
-install.packages("RSelenium")#for interacting with the webpage
+#install.packages("RSelenium")#for interacting with the webpage
 install.packages('sentimentr') # for sentiment analysis
 install.packages('quanteda') #further text mining and nlp analysis
 install.packages('quanteda.textplots') # for textplots
