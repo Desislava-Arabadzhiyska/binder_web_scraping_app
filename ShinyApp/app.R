57333249c6c3a3b0dced83226588d6bca1931e82
Desislava-Arabadzhiyska <33028920+Desislava-Arabadzhiyska@users.noreply.github.com>
2022-11-20 22:49:49 +0000

    Update app.R

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -101,32 +101,16 @@ server <- function(input, output) {
     
     xtra_words <- c("much", "can", "just", "get", "also", "may", "really", "make", "go", "thing")#to be removed
     
-    #storage
-    
-    # mydfm <- list()
-    # freq_dat <- list()
-    # model.stm <- list()
-    # reduced_comments <-list()
-    # ncs <- list()
-    
-    #suc <- FALSE
-    #p <- 4567
-    #while (suc == FALSE){
-    #tryCatch(
-     # {
+ 
+
+  
         rs_driver_object <- rsDriver(browser = 'firefox'#,
                                      #chromever = '106.0.5249.61',
                                      #port = as.integer(p), 
                                      #version = '4.0.0-alpha-2'#, 
                                      #geckover = "0.31.0"
                                     )
-      #},
-      #error=function(e){}
-    #)
-     # suc <- exists('rs_driver_object')
-     # p <- p + 1
-      #print(p)
-    #}
+     
     #activate client
     remDr <- rs_driver_object$client
     
